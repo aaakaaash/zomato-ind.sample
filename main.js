5d63d0f334124c7332786edf01e916bd142b1425
aaakaaash <akashravikumar2015@gmail.com>
2024-05-24 02:31:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,25 @@
 let mobileMenuIcon = document.querySelector("header .fa-bars")
 let menu = document.querySelector("header .navbar")
 mobileMenuIcon.addEventListener("click",function(){
-    menu.classList.toggle("open");
+    menu.classList.add("open");
 })
 mobileMenuIcon.addEventListener("click",function(){
-    this.classList.toggle("fa-bars-active");
+    mobileMenuIcon.classList.add("fa-bars-active");
+})
+mobileMenuIcon.querySelector("header .fa-x")
+{
+    mobileMenuIcon.addEventListener("click",function(){
+        mobileMenuIcon1.classList.add("fa-x-active");
+    })  
+}
+let mobileMenuIcon1 = document.querySelector("header .fa-x")
+mobileMenuIcon1.addEventListener("click", function(){
+menu.classList.remove("open")
+mobileMenuIcon1.classList.remove("fa-x-active")
+}
+)
+
+mobileMenuIcon1.querySelector("header .fabars")
+mobileMenuIcon1.addEventListener("click",function(){
+    mobileMenuIcon.classList.remove("fa-bars-active");
 })
